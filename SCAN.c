c7c8e11b8a73df2a1f7e2de1f3dc88d772a30dc5
Elvin Vincent A <84258439+ElvinVincent22@users.noreply.github.com>
2022-10-28 12:08:10 +0530

    Create SCAN.c

---
+++ SCAN.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int inihead, n, totheadmov= 0, i, totsize, dir, j, temp, index;
+    printf("No. of Requests:");
+    scanf("%d", &n);
+    
+    int rq[n];
+    printf("Request sequence:\n");
+    for(i= 0; i< n; i++)
+        scanf("%d", &rq[i]);
+
+    printf("Initial head position: ");
+    scanf("%d", &inihead);
+
+    printf("Total disk size: ");
+    scanf("%d", &totsize);
+    
+    printf("Head movement direction (1:high & 0:low) : ");
+    scanf("%d", &dir);
+
+   
+    for(i= 0; i< n; i++)
+    {
+        for(j= 0; j< n- i- 1; j++)
+        {
+            if(rq[j]> rq[j+ 1])
+            {
+                temp= rq[j];
+                rq[j]= rq[j+ 1];
+                rq[j+ 1]= temp;
+            }
+        }
+    }
+
+    for(i= 0; i< n; i++)
+    {
+        if(rq[i]> inihead)
+        {
+            index= i;
+            break;
+        }
+    }
+
+   
+    if(dir == 1)
+    {
+        for(i= index; i< n; i++)
+        {
+            totheadmov += abs(rq[i]- inihead);
+            inihead= rq[i]; 
+        }
+        
+        totheadmov += abs((totsize- 1)- inihead);
+        inihead= totsize- 1;
+        
+        for(i= index- 1; i>= 0; i--)
+        {
+            totheadmov += abs(rq[i]- inihead);
+            inihead= rq[i];
+        }
+    }
+
+    if(dir == 0)
+    {
+        for(i= index- 1; i>= 0; i--)
+        {
+            totheadmov += abs(rq[i]- inihead);
+            inihead= rq[i]; 
+        }
+        
+        totheadmov += abs(0- inihead);
+        inihead= 0;
+       
+        for(i= index; i< n; i++)
+        {
+            totheadmov += abs(rq[i]- inihead);
+            inihead= rq[i];
+        }
+    }
+    printf("Total head movement = %d\n", totheadmov);
+    return 0;
+}
